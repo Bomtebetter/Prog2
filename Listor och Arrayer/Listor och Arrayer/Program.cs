d55e6b00058fc09ca742bd03a6efe8ba859007ff
Bomtebetter <petter.bratland@elev.ga.ntig.se>
2020-09-11 11:37:33 +0200

    Orkar inte mer

---
+++ Listor och Arrayer/Listor och Arrayer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Listor_och_Arrayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] namn= new string[5];
+
+            Console.WriteLine("Write 5 names in 5 different inputs");
+            for(int i = 0; i < 5; i++)
+            {
+                namn[i] = Console.ReadLine();
+            }
+            
+            for (int i = 4; i > -1; i--)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
